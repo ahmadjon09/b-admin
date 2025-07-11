cac462c925682a769bc7174b6a1e201ba614818c
ahmadjon09 <youtobe83a61a@gmail.com>
2025-07-11 21:38:57 +0500

    first commit

--- src/middlewares/fetch.jsx
+++ src/middlewares/fetch.jsx
@@ -2,9 +2,9 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 
 const token = Cookies.get('admin_token')
-// const BASE_URL = 'vhttps://b-market-server.onrender.com/api/'
+const BASE_URL = 'vhttps://b-market-server.onrender.com/api/'
 
-const BASE_URL = 'http://localhost:8000/api/'
+// const BASE_URL = 'http://localhost:8000/api/'
 
 const instance = axios.create({
   baseURL: BASE_URL,
